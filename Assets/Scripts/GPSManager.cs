cb2e8c0a57dc4bb47612c0d5f9bb10ae9aa53ba7
joontohub <joonto7828@gmail.com>
2020-09-11 13:22:46 +0900

    Setting for Unity

---
+++ Assets/Scripts/GPSManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class GPSManager : MonoBehaviour
+{
+    
+    public InputField NameInput;
+    public InputField AgeInput;
+    public InputField SexInput;
+    public InputField PhoneInput;
+    public Button SendButton;
+    // Start is called before the first frame update
+    public static string NameData;
+    public static string AgeData;
+    public static string SexData;
+    public static string PhoneData;
+    void Start()
+    {
+        StartCoroutine("test");
+    }
+
+    IEnumerator test()
+    {
+        yield return new WaitForSeconds(1);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SaveData()
+    {
+        NameData = NameInput.text;
+        AgeData = AgeInput.text;
+        SexData = SexInput.text;
+        PhoneData = PhoneInput.text;
+
+        Debug.Log(NameData);
+        Debug.Log(AgeData);
+        Debug.Log(SexData);
+        Debug.Log(PhoneData);
+    }
+    public void CheckGPSConnect()
+    {
+        // if(!Input.location.isEnabledByUser)
+        // {
+        //     Debug.Log("User has not enable GPS");
+        // }
+    }
+    public void ConnectToGPS()
+    {
+        StartCoroutine(ConnectGPSCO());
+    }
+    IEnumerator ConnectGPSCO()
+    {
+        yield return new WaitForSeconds(1);
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+#This project is made by joonto
+##this project is tracking user's GPS Location to notice where corona patients went by.
+
